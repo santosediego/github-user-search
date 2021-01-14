482f155910ff43fa2c9a5262cfac6c442b5dd1e7
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-01-14 07:28:06 -0300

    Responsibility for the Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,16 +5,19 @@ import "./styles.css";
 
 const Home = () => (
     <div className="home-container">
+
         <h1 className="text-title">Desafio do Capítulo 3,<br /> Bootcamp DevSuperior</h1>
+
         <p className="text-description">
             Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior.
-        </p>
+            </p>
         <p className="text-description">
             Favor observar as instruções passadas no capítulo sobre a elaboração deste projeto.
-        </p>
+            </p>
         <p className="text-description">
             Este design foi adaptado a partir de Ant Design System for Figma, de Mateusz Wierzbicki: <span className="text-email">antforfigma@gmail.com</span>
         </p>
+
         <Link to="/search">
             <div className="btn-comecar">
                 <Button text="Começar" />
